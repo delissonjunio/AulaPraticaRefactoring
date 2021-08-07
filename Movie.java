8c63d6cc3ed16b1d123f46aabea24cec36fcd9fc
Délisson Silva <delisonjunio@gmail.com>
2021-08-07 16:34:57 -0300

    Commit 10

--- Movie.java
+++ Movie.java
@@ -46,4 +46,12 @@ public class Movie {
         return thisAmount;
     }
 
+    public int getFrequentRenterPoints(int daysRented){
+        // add bonus for a two day new release rental
+        if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
+            return 2;
+        }
+
+        return 1;
+    }
 }
