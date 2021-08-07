c105e2b09578988f1646fcca0d0f817fc5acf93e
Délisson Silva <delisonjunio@gmail.com>
2021-08-07 16:40:44 -0300

    Commit 15

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -6,4 +6,13 @@ public class NewReleasePrice extends Price {
     public double getCharge(int daysRented){
         return daysRented * 3;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        // add bonus for a two day new release rental
+        if (daysRented > 1) {
+            return 2;
+        }
+
+        return 1;
+    }
 }
